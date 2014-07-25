3420dacfa571e36136ca8fe0cbcbf840bbfcc922
Gregory Nisbet <gregory.nisbet@gmail.com>
2014-07-24 17:53:17 -0700

    Update cachematrix.R
    replaced function stubs with actual code

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## An implementation of a matrix that caches its inverse to avoid unnecessary computation.
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## construct a matrix 
+makeCacheMatrix <- function(x=numeric()) {
+	# pseudo-object's private members go here
+	mat <- x
+	inv <- NULL
+	
+	set <- function (new.mat) {
+		mat <<- new.mat
+		inv <<- NULL
+	}
+	
+	get <- function () { mat }
+	
+	set.inv <- function (new.inv) { inv <<- new.inv }
+	
+	get.inv <- function () { inv }
+	
+	list(set = set, get = get, set.inv = set.inv, get.inv = get.inv)
 }
 
 
-## Write a short comment describing this function
+## compute the inverse of the matrix x, optional arguments get passed along to solve
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x,...) {
+	mat <- x$get()
+	inv <- solve(mat,...)
+	x$set.inv(inv)
+	return(inv);
 }
